2183959b0d5b89167b6615608ad2aa24676fad21
Peter C Rigby <rigbypc@gmail.com>
2014-10-09 13:46:29 -0400

    update readme with new instructions, add stackoverflow process to date

--- src/date.sql
+++ src/date.sql
@@ -4,14 +4,22 @@
 --alter table clt add column date timestamp with time zone;
 
 --one of these!
+
+--Recodoc
 --update clt set date = msg_date from channel_message m where cast(du as int) = m.id;
---update clt set date = to_timestamp(p.creationdate) from posts p where cast(du as int) = p.id; -- get rid of timestamp conversion
---update clt set date = p.creationdate from posts p where cast(du as int) = p.id;
---update clt set date = msg_date from limited_posts p where cast(du as int) = p.id;
+
 
 --updates take too long
 alter table clt rename to clt_delme;
+
+--One of these will fail, but we keep both so as not to have to modify them on each run
+
+--stackoverflow
+create unlogged table clt as select c.*, p.creationdate as date from clt_delme c, posts p where cast(du as int) = p.id
+
+--For email
 create unlogged table clt as select c.*, e.date as date from clt_delme c, email e where c.du = e.msg_id;
+
 --drop table clt_delme;
 
 
